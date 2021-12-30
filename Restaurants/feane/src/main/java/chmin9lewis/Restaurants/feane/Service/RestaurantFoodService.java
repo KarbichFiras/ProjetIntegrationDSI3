ee838df9ab8a1217af1aef6a52050ea1333dd7bc
layes-git <80711416+layes-git@users.noreply.github.com>
2021-12-30 04:08:44 +0100

    requestampping to the service RestaurantFood

--- Restaurants/feane/src/main/java/chmin9lewis/Restaurants/feane/Service/RestaurantFoodService.java
+++ Restaurants/feane/src/main/java/chmin9lewis/Restaurants/feane/Service/RestaurantFoodService.java
@@ -14,6 +14,7 @@ import chmin9lewis.Restaurants.feane.Metier.IFoodMetier;
 
 @RestController
 @CrossOrigin
+@RequestMapping("RestaurantFoodService")
 public class RestaurantFoodService {
 
 	@Autowired
