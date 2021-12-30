a783974eb3cecc11338e60a6c2392399d6cdedfa
bechahedfiras <firas_bechahed@hotmail.com>
2021-12-30 13:51:33 +0100

    homepage

--- Front End/Feane/src/app/app-routing.module.ts
+++ Front End/Feane/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule , Routes } from '@angular/router';
 import { AddMenuComponent } from './add-menu/add-menu.component';
+import { HomepageComponent } from './homepage/homepage.component';
 const routes : Routes =[
 
-  { path: '',   redirectTo: 'home', pathMatch: 'full' },
+  { path: '',   redirectTo: 'homepage', pathMatch: 'full' },
   {path :"addmenu" , component : AddMenuComponent },
+  {path :"homepage" , component : HomepageComponent },
 ];
 
 @NgModule({
--- Front End/Feane/src/app/app.module.ts
+++ Front End/Feane/src/app/app.module.ts
@@ -3,21 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { MenuComponent } from './home/menu/menu.component';
-import { FooterComponent } from './home/footer/footer.component'; 
+import { AddMenuComponent } from './add-menu/add-menu.component'; 
 
 import { AppRoutingModule } from './app-routing.module';
-import { FoodlayoutComponent } from './home/foodlayout/foodlayout.component'; 
-import { AddMenuComponent } from './add-menu/add-menu.component';
+
 import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MenuComponent,
-    FooterComponent,
-    FoodlayoutComponent,
+    
     AddMenuComponent,
     HomepageComponent,
     
