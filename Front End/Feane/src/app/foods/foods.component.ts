e245664eef6613c44f4539bb8155bc92e60f5899
ihebessaied <i.essaied@yahoo.fr>
2021-12-31 20:29:23 +0100

    inject food service to food ts

--- Front End/Feane/src/app/foods/foods.component.ts
+++ Front End/Feane/src/app/foods/foods.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FoodService } from '../shared/food.service';
 @Component({
   selector: 'app-foods',
   templateUrl: './foods.component.html',
-  styleUrls: ['./foods.component.css']
+  styleUrls: ['./foods.component.css'],
+  providers:[FoodService]
 })
 export class FoodsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private foodService:FoodService) { }
 
   ngOnInit(): void {
   }
--- Front End/Feane/src/app/shared/food.service.ts
+++ Front End/Feane/src/app/shared/food.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, Subject } from 'rxjs'; 
+import { map } from 'rxjs/operators';
+import { Food } from './food.model';
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
-
+  SelectedFood : Food;
+  foods:Food[];
   constructor() { }
 }
