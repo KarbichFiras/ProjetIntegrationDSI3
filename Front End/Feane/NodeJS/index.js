aa92856c64f11a184f874ac9aa0c2a507ade7d58
bechahedfiras <firas_bechahed@hotmail.com>
2022-01-01 16:02:00 +0100

    register done

--- Front End/Feane/NodeJS/index.js
+++ Front End/Feane/NodeJS/index.js
@@ -4,19 +4,39 @@ const cors = require('cors');
 
 const { mongoose } = require('./db.js');
 var menuController = require('./controllers/menuController');
-
+var userController = require('./controllers/userController');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: 'http://localhost:62667' }));
-
+app.use(cors({
+    origin:['http://localhost:62092','http://127.0.0.1:62092'],
+    credentials:true
+  }));
 app.listen(3000, () => console.log('Server started at port : 3000'));
 
 app.use('/menu', menuController);
+app.use('/users', userController);
+
 
 //food controller
 var foodController = require('./controllers/foodController');
 app.use('/food', foodController);
 
 
+var passport = require('passport');
+var session = require('express-session');
+app.use(session({
+    name:'myname.sid',
+    resave:false,
+    saveUninitialized:false,
+    secret:'secret',
+    cookie:{
+      maxAge:36000000,
+      httpOnly:false,
+      secure:false
+    },
+  }));
 
+ require('./passport-config');
+  app.use(passport.session());
+  app.use(passport.initialize());
--- Front End/Feane/src/app/register/register.component.ts
+++ Front End/Feane/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { data } from 'jquery';
+import { UserService } from '../shared/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm:FormGroup = new FormGroup({
+    email:new FormControl(null,[Validators.email,Validators.required]),
+    username:new FormControl(null,Validators.required),
+    password:new FormControl(null,Validators.required),
+    
+  })
+  constructor(private _router:Router,private _userService:UserService) { }
 
   ngOnInit(): void {
   }
+  moveToLogin(){
+    this._router.navigate(['/login']);
+    }
 
+
+    register(){
+      if(!this.registerForm.valid){
+        console.log('Invalid Form'); return;
+      }
+  
+      this._userService.register(JSON.stringify(this.registerForm.value))
+    .subscribe(
+      data=> {console.log(data); this._router.navigate(['/login']);},
+      error=>console.error(error)
+    )
+     // console.log(JSON.stringify(this.registerForm.value));
+    }
 }
